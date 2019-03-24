cc15fe98718aed9f67a7eb01e5614ae2288fb481
Tezan Sahu <tezansahu@gmail.com>
2019-03-24 12:34:30 +0530

    Simple login/logout

---
+++ SimpleEmailClient.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <exception>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+int main(int argc, char *args[]){
+
+    // Check the correct number of arguements
+    if(argc != 4){
+        cerr<<"Incorrect number of arguments passed!\n";
+        exit(1);
+    }
+
+    // Get IP Address and Port Number of destination address 
+    char *address;
+    address = args[1];
+    char* token = strtok(address, ":");
+    const char* ip_address;
+    int portNum;
+    const char* _tokens[2];
+    for(int i=0;token!=NULL;i++){
+        _tokens[i] = token;
+        token = strtok(NULL,":");
+    }
+    ip_address = _tokens[0];
+    portNum = atoi(_tokens[1]);
+
+    // Get username and password
+    const char* userName = args[2];
+    const char* passwd = args[3];
+
+    // Define the client socket
+    int client_socket;
+    client_socket = socket(AF_INET, SOCK_STREAM,0);
+
+    // specify an address for the socket
+    struct sockaddr_in remote_address;
+    remote_address.sin_family = AF_INET;
+    remote_address.sin_port = htons(portNum);
+    inet_aton(ip_address, &remote_address.sin_addr);
+
+    // Connect to the remote server
+    int connection_status = connect(client_socket, (struct sockaddr *) &remote_address, sizeof(remote_address));
+    if(connection_status == -1){
+        printf("There was an error making a connection to the remote server\n\n");
+        exit(2);
+    }
+    cout<<"Connect Done: "<<address<<"\n";
+
+    // Send login details to the server
+    string request = (string("User: ")+ string(userName) + string(" Pass: ")+ string(passwd)+ string("\0")) ;
+    char response[4096] = "\0";
+    send(client_socket, request.c_str(), strlen(request.c_str()), 0);
+
+    // Handle message received from the server appropriately
+    if(recv(client_socket, response, sizeof(response), 0) >= 0){
+        printf("%s\n",response);
+        if(strcmp(response, (string("Welcome ")+string(userName)+ string("\n")).c_str()) != 0){
+            close(client_socket);
+            exit(1);
+        }
+    }
+    else{
+        close(client_socket);
+        exit(1);
+    }
+    
+    //After a brief pause, send a 'quit' request
+    sleep(1);
+    string quit = string("quit\0");
+    send(client_socket,quit.c_str(), strlen(quit.c_str()),0);
+    char response2[4096] = "\0";
+    recv(client_socket, response2, sizeof(response2),0);
+    close(client_socket);
+
+    return 0;
+}
---
+++ SimpleEmailServer.cpp
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <exception>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+
+#include <map>
+#include <iterator>
+#include <regex>
+
+using namespace std;
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+if (sa->sa_family == AF_INET) {
+return &(((struct sockaddr_in*)sa)->sin_addr);
+}
+return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char *args[]){
+    // Check the correct number of arguements
+    if(argc != 3){
+        cerr<<"Incorrect number of arguments passed!\n";
+        return 1;
+    }
+
+    const char* portNum = args[1];
+    const char* passwdfile = args[2];
+
+    // Open the password file and try to read the data and store it in a mapping
+    ifstream file;
+    map<string, string> data;
+    map<string, string>::iterator itr;
+    try{
+        file.open(passwdfile);
+        if(file.good() == 0){
+            cerr<<"Unable to open password file\n";
+            return 3;
+        }
+        while(file){
+            char line[100];
+            file.getline(line, 100);
+            char* token = strtok(line, " ");
+            const char* _tokens[2];
+            int i = 0;
+            for(int i=0;token!=NULL;i++){
+                _tokens[i] = token;
+                token = strtok(NULL," ");
+            }
+            data.insert(pair<string, string>(string(_tokens[0]), string(_tokens[1])));
+        }
+        
+    }
+    catch(exception& e){
+        cerr<<e.what()<<"\n";
+        return 3;
+    }
+
+    
+
+    // Define the server socket
+    struct sockaddr_storage their_addr;
+    char s[INET6_ADDRSTRLEN];
+    int server_socket;
+    server_socket = socket(AF_INET, SOCK_STREAM,0);
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(atoi(portNum));
+    
+    // Bind the server socket to the port entered
+    if(bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address))<0){
+        cerr<<"Bind Unsuccessful\n";
+        exit(2);
+    }
+    cout<<"Bind Done: "<<portNum<<"\n";
+
+    
+    // Start listening on the port for connections
+    if(listen(server_socket,10)<0){
+        cerr<<"Listen Unsuccessful\n";
+        exit(2);
+    }
+    cout<<"Listen Done: "<<portNum<<"\n";
+
+    int client_socket;
+
+    while(1){
+        socklen_t sin_size = sizeof their_addr;
+
+        // Accept connection from a client
+        client_socket = accept(server_socket,(struct sockaddr *)&their_addr, &sin_size);
+
+        if(client_socket < 0){
+            cerr<<"Accept error";
+            continue;
+        }
+
+        // Get the details of the client connected
+        char response[4096] = "\0";
+        inet_ntop(their_addr.ss_family,get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+        
+        // Receive the login details from the client
+        recv(client_socket, response, sizeof(response),0);
+        printf("Client: %s:%s\n", s,portNum);
+
+        // Compare the format of message received 
+        regex r("^User: .* Pass: .*$");
+        if(! regex_match(response, r)){
+            cout<<"Unknown Command\n";
+            close(client_socket);
+            continue;
+        }
+
+        // Get the username and password from the message received
+        char* token = strtok(response, " ");
+        const char* _tokens[4];
+        for(int i=0;token!=NULL;i++){
+            _tokens[i] = token;
+            token = strtok(NULL," ");
+        }
+        const char* input_userName = _tokens[1];
+        const char* input_passwd = _tokens[3];
+
+        // Check if the username is valid
+        itr = data.find(input_userName);
+        if(itr == data.end()){
+            string error  = "Invalid User\n";
+            cout<<error;
+            send(client_socket, error.c_str(), strlen(error.c_str()), 0);
+            close(client_socket);
+            continue;
+        }
+
+        // Check if the password is correct
+        if(itr->second != input_passwd){
+            string error  = "Wrong Password\n";
+            cout<<error;
+            send(client_socket, error.c_str(), strlen(error.c_str()), 0);
+            close(client_socket);
+            continue;
+        }
+
+        // Welcome the client
+        string welcome_msg = string("Welcome ")+string(input_userName)+string("\n");
+        cout<<welcome_msg;
+        send(client_socket, welcome_msg.c_str(), strlen(welcome_msg.c_str()), 0);
+
+        // Get the next message from the client
+        char response2[4096] = "\0";
+        recv(client_socket, response2, sizeof(response2),0);
+
+        // If the client sends 'quit' message, logout and close connections
+        if(strcmp(response2, "quit") == 0){
+            string logout_msg = string("Bye ")+string(input_userName)+string("\n");
+            cout<<logout_msg;
+            send(client_socket, logout_msg.c_str(), strlen(logout_msg.c_str()), 0);
+            close(client_socket);
+            continue;
+        }
+        else{
+            string logout_msg = string("Unknown Command\n");
+            cout<<logout_msg;
+            send(client_socket, logout_msg.c_str(), strlen(logout_msg.c_str()), 0);
+            close(client_socket);
+            continue;
+        }
+       
+        close(server_socket);
+    }
+
+    file.close();
+    return 0;
+}
